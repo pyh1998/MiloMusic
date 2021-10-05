ed07cc87d2b24b010a49c1f1726b96406c8cb1d2
Yuhui Pang <u7211790@anu.edu.au>
2021-10-05 17:05:17 +0800

    Create json and class

---
+++ app/src/main/java/com/example/social_network_app/Basic_class/Music.java
@@ -0,0 +1,4 @@
+package com.example.social_network_app.Basic_class;
+
+public class Music {
+}
---
+++ app/src/main/java/com/example/social_network_app/Basic_class/User.java
@@ -0,0 +1,8 @@
+package com.example.social_network_app.Basic_class;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private int age;
+}
