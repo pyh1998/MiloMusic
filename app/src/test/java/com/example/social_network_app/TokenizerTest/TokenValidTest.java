1dd97bfe1cfb758e4fdf33336afdf4e4e178f0cb
Man Jin <u7167784@anu.edu.au>
2021-10-19 01:19:09 +0800

    write TokenValidTest

--- app/src/test/java/com/example/social_network_app/TokenizerTest/TokenValidTest.java
+++ app/src/test/java/com/example/social_network_app/TokenizerTest/TokenValidTest.java
@@ -12,8 +12,37 @@ import org.junit.Test;
 public class TokenValidTest {
     @Test
     public void validTest(){
-        String piece1 = "#tag1";
+        /*
+        "#" followed by music tag,
+        "@" followed by name,
+        "*>" or "*>=" or "*<" or "*<=" or "*=" followed by music rate,
+        "XXX" type directly means search music name.
+         */
+        String piece1 = "#blue";
+        String piece2 = "@justin bieber";
+        String piece3 = "*>4.3";
+        String piece4 = "*4.3";
+        String piece5 = "*>>4.3";
+        String piece6 = "*==4.3";
+        String piece7 = "*4..3";
+        String piece8 = "*4.3.";
+        String piece9 = "*country";
+        String piece10 = "#";
+        String piece11 = "@";
+        String piece12 = "*";
 
         assertTrue(MusicToken.isValid(piece1));
+        assertTrue(MusicToken.isValid(piece2));
+        assertTrue(MusicToken.isValid(piece3));
+        assertFalse(MusicToken.isValid(piece4));
+        assertFalse(MusicToken.isValid(piece5));
+        assertFalse(MusicToken.isValid(piece6));
+        assertFalse(MusicToken.isValid(piece7));
+        assertFalse(MusicToken.isValid(piece8));
+        assertFalse(MusicToken.isValid(piece9));
+        assertFalse(MusicToken.isValid(piece10));
+        assertFalse(MusicToken.isValid(piece11));
+        assertFalse(MusicToken.isValid(piece12));
+
     }
 }
